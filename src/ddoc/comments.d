82b7942adabda713e74997a8c3de4560cdc72405
Hackerpilot <briancschott@gmail.com>
2015-09-23 21:06:02 -0700

    Minor code cleanup and formatting

--- src/ddoc/comments.d
+++ src/ddoc/comments.d
@@ -3,7 +3,6 @@
  * Authors: Brian Schott
  * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt Boost License 1.0)
  */
-
 module ddoc.comments;
 import ddoc.sections;
 import ddoc.lexer;
@@ -17,27 +16,30 @@ Comment parseComment(string text, string[string] macros)
 	auto sections = splitSections(text);
 	string[string] sMacros = macros;
 	auto m = sections.find!(p => p.name == "Macros");
-	auto e = sections.find!(p => p.name == "Escapes");
+	const e = sections.find!(p => p.name == "Escapes");
 	auto p = sections.find!(p => p.name == "Params");
-	if (m.length) {
+	if (m.length)
+	{
 		if (!doMapping(m[0]))
 			throw new DdocParseException("Unable to parse Key/Value pairs", m[0].content);
 		foreach (kv; m[0].mapping)
 			sMacros[kv[0]] = kv[1];
 	}
-	if (e.length) {
+	if (e.length)
+	{
 		assert(0, "Escapes not handled yet");
 	}
-	if (p.length) {
+	if (p.length)
+	{
 		if (!doMapping(p[0]))
 			throw new DdocParseException("Unable to parse Key/Value pairs", m[0].content);
 		foreach (ref kv; p[0].mapping)
 			kv[1] = expand(Lexer(kv[1]), sMacros);
 	}
 
-	foreach (ref Section sec; sections) {
-		if (sec.name != "Macros" && sec.name != "Escapes"
-		    && sec.name != "Params")
+	foreach (ref Section sec; sections)
+	{
+		if (sec.name != "Macros" && sec.name != "Escapes" && sec.name != "Params")
 			sec.content = expand(Lexer(highlight(sec.content)), sMacros);
 	}
 	return Comment(sections);
@@ -56,14 +58,18 @@ struct Comment
 	bool isDitto() const @property
 	{
 		import std.string : strip, toLower;
+
 		return sections.length == 2 && sections[0].content.strip().toLower() == "ditto";
 	}
+
 	Section[] sections;
 }
 
 unittest
 {
-	auto macros = ["A": "<a href=\"$0\">"];
+	import std.conv : text;
+
+	auto macros = ["A" : "<a href=\"$0\">"];
 	auto comment = `Best-comment-ever © 2014
 
 I thought the same. I was considering writing it, actually.
@@ -81,17 +87,13 @@ Returns:
 `;
 
 	Comment c = parseComment(comment, macros);
-//	writeln(c.sections.length);
-//	foreach (s; c.sections)
-//		writeln(s);
 	import std.string : format;
-//	writeln(c.sections);
+
 	assert(c.sections.length == 4, format("%d", c.sections.length));
 	assert(c.sections[0].name is null);
 	assert(c.sections[0].content == "Best-comment-ever © 2014", c.sections[0].content);
 	assert(c.sections[1].name is null);
 	assert(c.sections[2].name == "Params");
-//	writeln(c.sections[2].mapping);
 	assert(c.sections[2].mapping[0][0] == "a");
 	assert(c.sections[2].mapping[0][1] == `<a href="param">`, c.sections[2].mapping[0][1]);
 	assert(c.sections[3].name == "Returns");
@@ -105,13 +107,13 @@ auto subcube(T...)(T values);
 Creates a new cube in a similar way to whereCube, but allows the user to
 define a new root for specific dimensions.`c;
 	string[string] macros;
-	Comment c = parseComment(comment, macros);
+	const Comment c = parseComment(comment, macros);
 }
 
 ///
-unittest {
+unittest
+{
 	import std.conv : text;
-	import ddoc.comments;
 
 	auto s1 = `Stop the world
 
@@ -136,7 +138,7 @@ Nothing, because nobody can restart it.
 Macros:
 F= $0`;
 
-	auto expected = `<pre class="d_code"><font color=blue>void</font> main() {
+	immutable expected = `<pre class="d_code"><font color=blue>void</font> main() {
   <font color=blue>import</font> std.datetime : msecs;
   <font color=blue>import</font> master.universe.control;
   stopTheWorld(<font color=red>"Too fast"</font>, 42.msecs);
@@ -150,22 +152,26 @@ F= $0`;
 	assert(c.sections[0].content == "Stop the world", c.sections[0].content);
 
 	assert(c.sections[1].name is null, c.sections[1].name);
-	assert(c.sections[1].content == `This function tells the Master to stop the world, taking effect immediately.`,
-	       c.sections[1].content);
+	assert(
+		c.sections[1].content == `This function tells the Master to stop the world, taking effect immediately.`,
+		c.sections[1].content);
 
 	assert(c.sections[2].name == "Params", c.sections[2].name);
-//	writeln(c.sections[2].mapping);
+	//	writeln(c.sections[2].mapping);
 	assert(c.sections[2].mapping[0][0] == "reason", c.sections[2].mapping[0][0]);
-	assert(c.sections[2].mapping[0][1] == "Explanation to give to the <b>Master</b>", c.sections[2].mapping[0][1]);
+	assert(c.sections[2].mapping[0][1] == "Explanation to give to the <b>Master</b>",
+		c.sections[2].mapping[0][1]);
 	assert(c.sections[2].mapping[1][0] == "duration", c.sections[2].mapping[0][1]);
-	assert(c.sections[2].mapping[1][1] == "Time for which the world would be stopped (as time itself stop, this is always double.infinity)",
-	       c.sections[2].mapping[1][1]);
+	assert(
+		c.sections[2].mapping[1][1] == "Time for which the world would be stopped (as time itself stop, this is always double.infinity)",
+		c.sections[2].mapping[1][1]);
 
 	assert(c.sections[3].name == "Examples", c.sections[3].name);
 	assert(c.sections[3].content == expected, c.sections[3].content);
 
 	assert(c.sections[4].name == "Returns", c.sections[4].name);
-	assert(c.sections[4].content == `Nothing, because nobody can restart it.`, c.sections[4].content);
+	assert(c.sections[4].content == `Nothing, because nobody can restart it.`,
+		c.sections[4].content);
 
 	assert(c.sections[5].name == "Macros", c.sections[5].name);
 	assert(c.sections[5].mapping[0][0] == "F", c.sections[5].mapping[0][0]);
@@ -174,7 +180,7 @@ F= $0`;
 
 unittest
 {
-	import std.stdio:writeln, writefln;
+	import std.stdio : writeln, writefln;
 
 	auto comment = `Unrolled Linked List.
 
@@ -196,12 +202,14 @@ Params:
 }
 
 private:
-bool doMapping(ref Section s) {
+bool doMapping(ref Section s)
+{
 	import ddoc.macros : KeyValuePair, parseKeyValuePair;
 
 	auto lex = Lexer(s.content);
 	KeyValuePair[] pairs;
-	if (parseKeyValuePair(lex, pairs)) {
+	if (parseKeyValuePair(lex, pairs))
+	{
 		foreach (idx, kv; pairs)
 			s.mapping ~= kv;
 		return true;
